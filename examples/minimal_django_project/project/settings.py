32b24b9543154d68d00cb04f4576d7675a9d1abb
Ignace Maes <10243652+IgnaceMaes@users.noreply.github.com>
2026-02-22 15:24:18 +0100

    fix: test app

--- examples/minimal_django_project/project/settings.py
+++ examples/minimal_django_project/project/settings.py
@@ -26,7 +26,20 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "project.urls"
 
-TEMPLATES = []
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    }
+]
 WSGI_APPLICATION = "project.wsgi.application"
 ASGI_APPLICATION = "project.asgi.application"
 
